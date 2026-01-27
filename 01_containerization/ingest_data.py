d13a989bd5180c900b7f3546a2e8cc7e0803fab1
Dardo Daniel Iramain <danieliramain97@gmail.com>
2026-01-27 22:27:33 +0000

    Homework 1 and IaC with Terraform

--- 01_containerization/ingest_data.py
+++ 01_containerization/ingest_data.py
@@ -26,8 +26,8 @@ dtype = {
 }
 
 parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
+    "lpep_pickup_datetime",
+    "lpep_dropoff_datetime"
 ]
 
 
@@ -37,20 +37,19 @@ parse_dates = [
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--year', default=2021, type=int, help='Year of the data')
-@click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', default=2025, type=int, help='Year of the data')
+@click.option('--month', default=11, type=int, help='Month of the data')
+@click.option('--target-table', default='green_taxi_data', help='Target table name')
 @click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
 
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     """Ingest NYC taxi data into PostgreSQL database."""
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    file = f'data/green_tripdata_{year}-{month:02d}.csv'
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     df_iter = pd.read_csv(
-        url,
+        file,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
